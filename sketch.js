25fc0bd8852197b5081db6ad6d96e42a546c1be3
unknown <edward2000ed@gmail.com>
2021-07-01 11:59:57 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,149 @@
+
+
+let font;
+let text;
+let vehicles = [];
+let points;
+let letterPoints = [];
+let alphaSlider;
+let fontsizeSlider;
+let bgButton;
+let resetButton;
+let bgcolor = 255;
+let spacing = 20;
+
+function preload() {
+    font = loadFont("font.ttf");
+}
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+    text = prompt("Enter Name of your loved one");
+    if(text === null) {
+        text = "Edward";
+    }
+    alert("Note:\nTry to touch the text.\nDouble click and hold anywhere on website to drive particles in path\nTweak the slider values and have fun");
+    alphaSlider = createSlider(0,255,255,5);
+    alphaSlider.position(width/3,20);
+    alphaSlider.style("width",width/2 + "px");
+
+    fontsizeSlider = createSlider(40,200,100,5);
+    fontsizeSlider.position(width/3,40);
+    fontsizeSlider.style("width",width/2 + "px");
+
+    bgButton = createButton("ChangeBG");
+    bgButton.position(20, 20);
+    bgButton.id("bgButton");
+
+    resetButton = createButton("Reset");
+    resetButton.position(20, 60);
+    resetButton.id("resetButton");
+
+    resetButton.mousePressed(function() {
+        if(vehicles[0][0]) {
+            for(vArray of vehicles) {
+                for (v of vArray) {
+                    v.x = random(width);
+                    v.y = random(height);
+                }
+            }
+        }
+       
+    })
+
+    bgButton.mousePressed(function() {
+        if(bgcolor == 255) {
+            bgcolor = 0
+        } else {
+            bgcolor = 255;
+        }
+    })
+
+    let cycle = 0;
+        let lastpoint = width / 8;
+        let fontSize = fontsizeSlider.value();
+        for(letter of text) {
+        letterPoints.push(font.textToPoints(letter,lastpoint + spacing, height/1.5,fontSize));
+
+        if(letterPoints[cycle].length == 0) {
+            lastpoint = lastpoint + 10;
+            cycle++;
+            continue;
+            }
+
+        for(pt of letterPoints[cycle]) {
+            if(pt.x > lastpoint) {
+                lastpoint = pt.x;
+            }
+        }
+        cycle++;
+        }
+        cycle = 0;
+        for(points of letterPoints) {
+            vehicles[cycle] = [];
+            for(pt of points) {
+                vehicles[cycle].push(new Vehicle(pt.x, pt.y, fontSize))
+            }
+            cycle++;
+        }
+
+    fontsizeSlider.mouseClicked(function() {
+        vehicles = [];
+        letterPoints = [];
+        let cycle = 0;
+        let lastpoint = width / 8;
+        let fontSize = fontsizeSlider.value();
+        for(letter of text) {
+        letterPoints.push(font.textToPoints(letter,lastpoint + spacing, height/1.5,fontSize));
+
+        if(letterPoints[cycle].length == 0) {
+            lastpoint = lastpoint + 10;
+            cycle++;
+            continue;
+            }
+
+        for(pt of letterPoints[cycle]) {
+            if(pt.x > lastpoint) {
+                lastpoint = pt.x;
+            }
+        }
+        cycle++;
+        }
+        
+        cycle = 0;
+        for(points of letterPoints) {
+            vehicles[cycle] = [];
+            for(pt of points) {
+                vehicles[cycle].push(new Vehicle(pt.x, pt.y, fontSize))
+            }
+            cycle++;
+        }
+
+    });
+
+}
+
+function draw() {
+    let alphaValue = alphaSlider.value();
+    background(bgcolor,alphaValue);
+    for(vArray of vehicles) {
+        for (v of vArray) {
+            v.behave();
+            v.update();
+            v.show();
+        }
+    }
+
+    if(frameCount % 10 == 0 && mouseIsPressed ) {
+        for(vArray of vehicles) {
+            if(vArray.length > 0) {
+                let last = vArray[0].target;
+                for(let i = 0; i < vArray.length - 1; i++) {
+                    vArray[i].target = vArray[i+1].target;
+                }
+                vArray[vArray.length - 1].target = last;
+            }   
+        }
+        
+    }
+}
---
+++ vehicle.js
@@ -0,0 +1,86 @@
+
+//check coding train text challenge video to know how the vehicle class works
+//sorry I didn't give a link, just go to youtube and search for coding train :)...booom!
+
+class Vehicle extends p5.Vector {
+
+    constructor(x, y, size) {
+        super(random(width), random(height));
+        this.target = createVector(x, y);
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0,0);
+        this.mass = 1;
+        this.maxSpeed = 15;
+        this.maxForce = 1;
+        this.r = sqrt(this.mass) * 16;
+        this.colour = [random(255), random(255), random(255)];
+        this.strokeSize = map(size, 40, 200, 4, 14)
+    }
+
+    behave() {
+        this.arrive(this.target);
+        this.flee(createVector(mouseX, mouseY));
+    }
+
+    arrive(target) {
+        let force = p5.Vector.sub(target, this);
+        let d = force.mag();
+        let speed = this.maxSpeed;
+        if(d < 100) {
+            speed = map(d, 0, 100, 0, this.maxSpeed);
+        }
+        force.setMag(speed);
+        force.sub(this.vel);
+        force.limit(this.maxForce);
+        this.applyForce(force)
+    }
+
+    seek(target) {
+        let force = p5.Vector.sub(target, this);
+        force.setMag(this.maxSpeed);
+        force.sub(this.vel);
+        force.limit(this.maxForce);
+        this.applyForce(force)
+    }
+
+    flee(target) {
+        let force = p5.Vector.sub(target, this);
+        let d = force.mag();
+
+        if(d < 50) {
+            force.setMag(this.maxSpeed);
+            force.mult(-1);
+            force.sub(this.vel);
+            force.limit(this.maxForce);
+            force.mult(5);
+            this.applyForce(force)
+        }
+        
+    }
+
+  
+    applyForce(force) {
+        let f = p5.Vector.div(force,this.mass);
+        this.acc.add(f);
+      
+    }
+  
+    update() {
+  
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxSpeed);
+        this.add(this.vel);
+    
+        this.acc.set(0,0);
+    }
+
+    show() {
+        stroke(this.colour[0], this.colour[1], this.colour[2]);
+        strokeWeight(this.strokeSize);
+        point(this.x, this.y);
+        // image(img, this.x, this.y);
+    }
+  
+}
+  
+  
